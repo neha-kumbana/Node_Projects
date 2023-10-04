60db367c4cf48b49ed00ee547cb2d974f51813ee
Neha K R <nehakr@AD.NSEIT.COM>
2023-10-04 17:23:02 +0530

    ninth commit

--- JWT-Basics/starter/controllers/main.js
+++ JWT-Basics/starter/controllers/main.js
@@ -1,12 +1,30 @@
+const CustomAPIError = require('../errors/custom-error')
+const jwt = require('jsonwebtoken')
 
 const login = async(req,res) => {
-    res.send('Fake Login/Register/Signup Route')
+    const {username,password} = req.body
+    //we can use mongoose validation or
+    //Joi or
+    //check in the controller
+
+    if(!username || !password){
+        throw new CustomAPIError('Please provide username and password',400)
+    }
+    //just for demo, normally provided by DB
+    const id = new Date().getDate()
+
+    //try to keep payload small, better experience for user
+    //just for demo, in production use long, complex and unguessable string value
+    const token = jwt.sign({id,username},process.env.JWT_SECRET,{expiresIn:'30d'})
+
+    res.status(200).json({msg:'user created',token})
 } 
 
 const dashboard = async(req,res) => {
+    console.log(req.user);
     const luckyNumber = Math.floor(Math.random()*100)
-    res.status(200).json({msg:`Hello Neha`, secret:`Here is your authorized data, your lucky number is ${luckyNumber}`})
-
+    res.status(200).json({msg:`Hello, ${req.user.username} `, secret:`Here is your authorized data, your lucky number is ${luckyNumber}`})
+    
 }
 module.exports = {
     login,
